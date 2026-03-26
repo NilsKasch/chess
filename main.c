9d42bd24462d97ad13413efb267226155d4f4c30
Nils Kasch <kasch.nils@gmail.com>
2026-03-26 22:41:43 +0100

    first eval func

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+typedef struct {
+    char txt;
+    float value;
+    int x,y;
+}Piece;
+
+float eval(Piece *pieces){
+    float sum=0;
+    for (int i = 0; i < 16; i++) {
+        sum = sum + pieces[i].value;
+        //printf("%f\n",pieces[i].value);
+    }
+    for (int i = 16; i < 32; i++) {
+        sum = sum - pieces[i].value;
+        //printf("%d\n",i);
+    }
+    return sum;
+}
+
 int main (int argc, char *argv[]){
     if (argc == 2)
     {
@@ -8,49 +27,47 @@ int main (int argc, char *argv[]){
     else
     {
         printf("Usage :\n");
-        printf("%s <number of steps\n", argv[0]);
+        printf("%s <number of steps>\n", argv[0]);
+        return 1;
     }
 
-    typedef struct {
-        char txt;
-        float value;
-        int x,y;
-    }Piece;
-
     Piece pieces[32] = {
-        {'p',1,2,1},
+        {'p',1,1,2},
         {'p',1,2,2},
-        {'p',1,2,3},
-        {'p',1,2,4},
-        {'p',1,2,5},
-        {'p',1,2,6},
-        {'p',1,2,7},
-        {'p',1,2,8},
-        {'p',1,7,1},
+        {'p',1,3,2},
+        {'p',1,4,2},
+        {'p',1,5,2},
+        {'p',1,6,2},
         {'p',1,7,2},
-        {'p',1,7,3},
-        {'p',1,7,4},
-        {'p',1,7,5},
-        {'p',1,7,6},
-        {'p',1,7,7},
-        {'p',1,7,8},
+        {'p',1,8,2},
         {'R',5,1,1},
-        {'R',5,1,8},
         {'R',5,8,1},
-        {'R',5,8,8},
         {'N',3,2,1},
         {'N',3,7,1},
-        {'N',3,2,8},
-        {'N',3,7,8},
         {'B',3,3,1},
         {'B',3,6,1},
+        {'Q',9,4,1},
+        {'K',1000,5,1},
+        {'p',1,1,7},
+        {'p',1,2,7},
+        {'p',1,3,7},
+        {'p',1,4,7},
+        {'p',1,5,7},
+        {'p',1,6,7},
+        {'p',1,7,7},
+        {'p',1,8,7},
+        {'R',5,1,8},
+        {'R',5,8,8},
+        {'N',3,2,8},
+        {'N',3,7,8},
         {'B',3,3,8},
         {'B',3,6,8},
-        {'Q',9,4,1},
         {'Q',9,4,8},
-        {'K',1000,5,1},
         {'K',1000,5,8}
     };
 
+    printf("start\n");
+    printf("eval: %f\n",eval(pieces));
 
+    return 0;
 }
