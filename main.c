5cc1481d7b64bd5c5907e9f97e2cd055772d681f
Nils Kasch <kasch.nils@gmail.com>
2026-03-27 21:55:11 +0100

    quelques avancées

--- main.c
+++ main.c
@@ -26,6 +26,7 @@ float eval(Piece *pieces){
 }
 
 void update_grid(Piece *pieces, int grid[]){
+    // inpossible de supprimer cette boucle pour l'instant
     for (int i = 0; i < 64; i++) {
         grid[i]=32;
     }
@@ -37,12 +38,29 @@ void update_grid(Piece *pieces, int grid[]){
     }
 }
 
-void apply_move(Piece *pieces, int grid[], Move *move){
-    if (grid[(pieces[move->piece].x+(move->x))+(pieces[move->piece].y+(move->y))*8]!=32){
-        pieces[grid[(pieces[move->piece].x+(move->x))+(pieces[move->piece].y+(move->y))*8]].value=-1;
+void apply_move(Piece *pieces, int grid[], Move *move, Piece *undo_piece){
+    int target=grid[(pieces[move->piece].x+(move->x))+(pieces[move->piece].y+(move->y))*8];
+    if (target!=32){
+        undo_piece->txt = pieces[target].txt;
+        undo_piece->value = pieces[target].value;
+        undo_piece->x = target;
+        undo_piece->y = target;
+        pieces[target].value=-1;
     }
     pieces[move->piece].x += move->x;
     pieces[move->piece].y += move->y;
+    update_grid(pieces, grid);
+}
+
+void undo_move(Piece *pieces, int grid[], Move *move, Piece *undo_piece){
+    if (undo_piece->value > 0){
+        pieces[undo_piece->x].txt = undo_piece->txt;
+        pieces[undo_piece->x].value = undo_piece->value;
+        pieces[undo_piece->x].x = pieces[move->piece].x;
+        pieces[undo_piece->x].y = pieces[move->piece].y;
+    }
+    pieces[move->piece].x -= move->x;
+    pieces[move->piece].y -= move->y;
     update_grid(pieces, grid);
 }
 
@@ -74,16 +92,17 @@ Move next(short white, Piece *pieces, int grid[], Move move, int depth){
         // si la valeur est égale, on ajoute au tableau en vu de faire un move aléatoire
         // undo move
     Move best, tmp;
+    Piece undo_piece;
+    undo_piece.value=-1;
     best.value=-1000;
+    apply_move(pieces,grid,&move, &undo_piece);
     if (depth == 0){
         best.piece=move.piece;
         best.x=move.x;
         best.y=move.y;
-        best.value=eval_move(pieces,grid,move);
+        best.value=eval(pieces);
+        undo_move(pieces,grid,&move, &undo_piece);
         return best;
-    }
-    else{
-        apply_move(pieces,grid,&move);
     }
     for (int i = 8 - white*8 ; i < 24 - white*8 ; i++) {
         if (pieces[i].value < 0){
@@ -94,18 +113,15 @@ Move next(short white, Piece *pieces, int grid[], Move move, int depth){
                 tmp.x=0;
                 tmp.y=1*white;
                 tmp.piece=i;
-                tmp.value=0;
-                //apply_move
-                //pieces[i].y += 1*white;
-                //update_grid(pieces, grid);
-                tmp = next(white,pieces, grid, tmp, depth - 1);
+                tmp = next(-white,pieces, grid, tmp, depth - 1); //clacul value
+                tmp.x=0;
+                tmp.y=1*white;
+                tmp.piece=i;
                 keep(&best, &tmp, &white);
-                //undo_move
-                //pieces[i].y -= 1*white;
-                //update_grid(pieces, grid);
             }
         }
     }
+    undo_move(pieces,grid,&move, &undo_piece);
     return best;
 }
 
@@ -168,10 +184,10 @@ int main (int argc, char *argv[]){
         //printf("grid: %d\n",grid[i]);
     }
     
-    Move move;
+    Move move = {0,0,0,0};
     //main loop
     for (int i = 1; i < 4; i++) {
-        move = next(1,pieces,grid,2);
+        move = next(1,pieces,grid,move,2);
         printf("%d.%c%d%d\n",i,pieces[move.piece].txt, move.x, move.y);
     }
     
